c3dbdd80fc72122b91537c2a81dd8210e4fea92d
Luciana <lbferrand@gmail.com>
2020-08-16 02:24:34 -0400

    Correct files

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,5 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> Render-comments
   # GET /ideas
   # GET /ideas.json
   def index
@@ -14,11 +9,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
-<<<<<<< HEAD
-=======
     @comments = @idea.comments.all
     @comment = @idea.comments.build
->>>>>>> Render-comments
   end
 
   # GET /ideas/new
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,9 +1,4 @@
 class Idea < ApplicationRecord
-<<<<<<< HEAD
-mount_uploader :picture, PictureUploader
-=======
 	mount_uploader :picture, PictureUploader
-
 	has_many :comments
->>>>>>> Render-comments
 end
