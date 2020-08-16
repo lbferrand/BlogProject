2ecf0f2a89d03d55d70d9adaa9ff676ea4c4741b
Luciana <lbferrand@gmail.com>
2020-08-15 20:55:03 -0400

    Missing changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def login
+  end
+
+  def sign_in
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+	resources :posts do 
+		resources :comments
+	end
+
   get 'home/index'
 
-  resources :articles
+  #resources :articles
 
-  root 'home#index'
+  #root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
